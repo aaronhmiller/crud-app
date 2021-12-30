ff25252a2d76b503894d01cc57449b60d60df313
Aaron Miller <aaronmiller@gmail.com>
2021-12-29 23:11:02 -0800

    adding TLS

--- app/app.js
+++ app/app.js
@@ -1,9 +1,18 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express')
 const bodyParser = require('body-parser')
 const os = require('os')
 const app = express()
-const port = 3000
+const secPort = 3443
 const api = require('./apis')
+
+const options = {
+  key: fs.readFileSync('./keys/key.pem'),
+  cert: fs.readFileSync('./certs/cert.pem')
+};
+
+const httpsServer = https.createServer(options, app);
 
 app.use(bodyParser.json())
 app.use(
@@ -22,8 +31,8 @@ app.post('/users', api.createUser)
 app.put('/users/:id', api.updateUser)
 app.delete('/users/:id', api.deleteUser)
 
-app.listen(port, () => {
-  console.log(`App running in ${os.platform} on port ${port} on ${os.arch}.`)
+httpsServer.listen(secPort, () => {
+  console.log(`App running in ${os.platform} on port ${secPort} on ${os.arch}.`)
 })
 
 /* API doc
